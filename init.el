df2c0a47fe66b4455bb81f8360ad8e16c0d931cb
Abd El-Twab M. Fakhry <abdeltwab.m.fakhry@protonmail.com>
2022-03-09 03:27:07 +0200

    Add emacs application framework

--- init.el
+++ init.el
@@ -752,11 +752,28 @@
   (eaf-browser-enable-adblocker t)
   (browse-url-browser-function 'eaf-open-browser)
   :config
-  (defalias 'browse-web #'eaf-open-browser)
-  (eaf-bind-key scroll_up "C-n" eaf-pdf-viewer-keybinding)
-  (eaf-bind-key scroll_down "C-p" eaf-pdf-viewer-keybinding)
-  (eaf-bind-key take_photo "p" eaf-camera-keybinding)
-  (eaf-bind-key nil "M-q" eaf-browser-keybinding)) ;; unbind, see more in the Wiki
+  (defalias 'browse-web #'eaf-open-browser)) ;; unbind, see more in the Wiki
+
+(require 'eaf-browser)
+(require 'eaf-pdf-viewer)
+(require 'eaf-terminal)
+(require 'eaf-video-player)
+(require 'eaf-markdown-previewer)
+(require 'eaf-org-previewer)
+(require 'eaf-music-player)
+(require 'eaf-rss-reader)
+(require 'eaf-file-manager)
+(require 'eaf-mindmap)
+(require 'eaf-jupyter)
+(require 'eaf-image-viewer)
+(require 'eaf-camera)
+(require 'eaf-system-monitor)
+(require 'eaf-netease-cloud-music)
+(require 'eaf-file-browser)
+(require 'eaf-file-sender)
+(require 'eaf-airshare)
+(require 'eaf-demo)
+(require 'eaf-vue-demo)
 
 (use-package pdf-view-restore
   :after pdf-tools
